cca21eeaf3ab390bb255ec284e9e3f331c63c2a4
lubnayaqoob <lubnayaqoob@gmail.com>
2021-08-12 16:28:20 +0200

    allure report added

--- src/test/java/com/qa/qatask/pages/Dashboard.java
+++ src/test/java/com/qa/qatask/pages/Dashboard.java
@@ -1,12 +1,15 @@
 package com.qa.qatask.pages;
 
 import com.qa.qatask.base.TestBase;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 import util.TestUtil;
 
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.TimeUnit;
 
 public class Dashboard extends TestBase {
@@ -21,11 +24,35 @@ public class Dashboard extends TestBase {
     WebElement we_portal;
     //div[@class='u-ui-padding-regular u-fill-width']//div/descendant::span[text()='United Kingdom']
 
-    @FindBy(id="search_text")
+    @FindBy(id = "search_text")
     WebElement we_search;
 
-    @FindBy(xpath = "//div[@class='card free']//div[@class='card-title'][6]")
-    WebElement getPowerConsumedWE;
+    @FindBy(xpath = "//ul[@class='pile']//li/descendant::a[@href='/vetements?search_text=Nike']")
+    WebElement we_clickNike;
+
+    @FindBy(xpath = "//a[@class='c-tabs__link js-header-navigation-tab-item' and @href=\"/women\"]")
+    WebElement we_women;
+
+    @FindBy(xpath = "//a[@class='c-cell__link js-browse-tab' and @href=\"/women/shoes\"]")
+    WebElement we_womenShoes;
+
+    @FindBy(xpath = "//div[@data-id='543' and @data-title='Heels']")
+    WebElement we_womenShoesHeels;
+
+    @FindBy(xpath = "//a[@href=\"/women/shoes/heels/high-heels\"]")
+    WebElement we_womenShoesHeelsHighHeels;
+
+    @FindBy(xpath = "//span[@class=\"Button_content__1Pyrd\"]//span/descendant::span[text()='Clear filters']")
+    WebElement we_clearFilter;
+
+    @FindBy(xpath = "//span[@class=\"Button_label__3jCra\"]//span[text()='Sort by']")
+    WebElement we_sortBy;
+
+    @FindBy(xpath = "//span[text()='Price: Low to High']//parent::h2[@class='Text_text__QBn4- Text_title__2EQGT Text_left__3s3CR']")
+    WebElement we_lowToHighPrice;
+
+    @FindBy(xpath = "//li[@class=\"pile__element\"]//div/descendant::span[text()='Price: high to low']")
+    WebElement we_lowToHighPrice1;
 
 
     //constructor of the class
@@ -41,7 +68,7 @@ public class Dashboard extends TestBase {
     }
 
     //select Portal
-    public Boolean selectPortal(){
+    public Boolean selectPortal() {
         we_portal.click();
         return true;
     }
@@ -53,14 +80,72 @@ public class Dashboard extends TestBase {
     }
 
     //accept all cookies
-    public Boolean acceptCookies()  {
+    public Boolean acceptCookies() {
         we_acceptCookies.click();
         return true;
 
     }
 
-    public void search(){
+    //search with key 'Nik;
+    public void search() {
         we_search.sendKeys("Nik");
     }
+
+    //click on 'Nike'
+    public Boolean clickNike() {
+        we_clickNike.click();
+        return true;
+    }
+
+    //Nike text present
+    public Boolean NikeTextPresent() {
+        driver.getPageSource().contains("Nike");
+        return true;
+    }
+
+    //click on dropdown list and select  and click price filter
+    public Boolean selectWomenShoes() {
+        we_women.click();
+        we_womenShoes.click();
+        we_womenShoesHeels.click();
+        we_womenShoesHeelsHighHeels.click();
+        return true;
+    }
+
+    public Boolean clearFilter() throws InterruptedException {
+        Thread.sleep(5000);
+        we_clearFilter.click();
+        return true;
+    }
+
+    public Boolean sorByPrice() {
+        we_sortBy.click();
+        List<WebElement> priceList = driver.findElements(By.xpath("//ul[@class=\"pile\"]"));
+        for (WebElement element : priceList) {
+            if (element.getText().contains("Price: Low to High")) {
+                element.click();
+            }
+        }
+       return true;
+    }
+
+
+    public Boolean isSortedByPrice(){
+
+        List<WebElement> priceList = driver.findElements(By.xpath("//div[@class=\"ItemBox_title-content__1LClm\"]"));
+        List<Integer> values = new ArrayList<Integer>();
+
+
+            for (WebElement element : priceList) {
+                values.add(Integer.parseInt(element.getText()));
+
+            }
+            for( int j = 0; j <values.size(); j++)
+                System.out.println(" values.add(Integer.parseInt(element.getText()))======" + values.get(j));
+            //List ourAl = new ArrayList<>();
+        return true;
+        }
+
+
 
 }
--- src/test/java/com/qa/qatask/testCASES/DashboardTest.java
+++ src/test/java/com/qa/qatask/testCASES/DashboardTest.java
@@ -25,45 +25,90 @@ public class DashboardTest extends TestBase {
         initialization();
         dashoard = new Dashboard();
     }
-
-    @Test(priority = 3, description = "verify title page test")
-    @Severity(SeverityLevel.BLOCKER)
-    public void verifyTitle() {
-        String title = dashoard.title();
-        Assert.assertEquals("Vinted | Sell and buy clothes, shoes and accessories", title);
+    @Test(priority = 1, description = "close portal")
+    @Severity(SeverityLevel.MINOR)
+    public void verifycloseportal()  {
+        Boolean result = dashoard.closetPortal();
+        Assert.assertTrue(true);
     }
 
-    @Test(priority = 1)
+    @Test(enabled=false)
+    @Severity(SeverityLevel.NORMAL)
     public void verifyPortal()  {
         Boolean result = dashoard.selectPortal();
         Assert.assertTrue(true);
     }
 
-    @Test(enabled=false)
-    public void verifycloseportal()  {
-        Boolean result = dashoard.closetPortal();
-        Assert.assertTrue(true);
+    @Test(priority = 3, description = "verify title page test //warm up test")
+    @Severity(SeverityLevel.MINOR)
+    public void verifyTitle() {
+        String title = dashoard.title();
+        Assert.assertEquals("Vinted | Sell and buy clothes, shoes and accessories", title);
     }
-    @Test(priority = 4)
-    public void verifyacceptCookies() throws InterruptedException {
 
+    @Test(priority = 4, description = "verify Accept Cookies")
+    @Severity(SeverityLevel.MINOR)
+    public void verifyacceptCookies()  {
         Boolean result = dashoard.acceptCookies();
         Assert.assertTrue(true);
     }
 
-    @Test(priority = 5)
+    @Test(priority = 5, description = "write Nik in search bar")
+    @Severity(SeverityLevel.BLOCKER)
     public void verifySearch() {
         dashoard.search();
     }
 
+    @Test(priority = 6, description = "verify Nike is clikced")
+    public void verifyclickNike() {
+        dashoard.clickNike();
+        Assert.assertTrue(true);
+
+    }
+
+    @Test(priority = 7, description = "verify Text is present on page with Nike")
+    @Severity(SeverityLevel.CRITICAL)
+    public void verifyNikeTextPresent() {
+        dashoard.NikeTextPresent();
+        Assert.assertEquals(true , true);
+    }
+
+    @Test(priority = 8, description = "Assert that “Nike” filter appeared below the filter menu")
+    @Severity(SeverityLevel.NORMAL)
+    public void verifselectWomenShoes() {
+        dashoard.selectWomenShoes();
+        Assert.assertEquals(true , true);
+    }
+
+    @Test(priority = 9, description = "clear all filters")
+    @Severity(SeverityLevel.NORMAL)
+    public void verifclearFilter() throws InterruptedException {
+        dashoard.clearFilter();
+        Assert.assertEquals(true , true);
+    }
 
 
 
+    @Test(priority = 10, description = "clear all filters")
+    @Severity(SeverityLevel.NORMAL)
+    public void verifsorByPrice() {
+        dashoard.sorByPrice();
+        Assert.assertEquals(true , true);
+    }
 
-/*    @AfterTest
-    public void quitBrowserTest(){
-        tearDown();
-    }*/
 
+    @Test(priority = 11, description = "clear all filters")
+    @Severity(SeverityLevel.NORMAL)
+    public void verifyIsSortedByPrice() {
+        dashoard.isSortedByPrice();
+        Assert.assertEquals(true , true);
+    }
+
+
+   /*@AfterTest
+    public void quitBrowserTest() {
+       tearDown();
+   }
+*/
 
 }
